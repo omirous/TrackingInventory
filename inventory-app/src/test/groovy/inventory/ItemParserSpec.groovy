4b8fe53a97790b3cff6a8df3d24f2fc31f389601
Stelios Omirou <omirous@interamerican.gr>
2021-08-16 00:35:17 +0300

    added some comments in the ItemParserSpec

--- inventory-app/src/test/groovy/inventory/ItemParserSpec.groovy
+++ inventory-app/src/test/groovy/inventory/ItemParserSpec.groovy
@@ -4,17 +4,24 @@ import spock.lang.Specification
 
 class ItemParserSpec extends Specification {
 
+	ItemParser parser
+
+	def setup() {
+		parser = new ItemParser()
+	}
+
 	def "parse a null line" () {
-		given:
-		ItemParser parser = new ItemParser()
+		given: 'null input'
 		String input = null
 
-		when:
+		when: 'parsing that input to an Item'
 		parser.parseLine(input)
 
-		then:
+		then: 'an InvalidInput expection is thrown'
 		def e = thrown(InvalidInput)
 		e.message == "Expected a line with item data but got null instead."
 	}
+
+
 
 }
