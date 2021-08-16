1194dc5b87065059860167b265acbcd35dd86d67
Stelios Omirou <omirous@interamerican.gr>
2021-08-16 23:40:59 +0300

    Comments for InvalidItem exception public API.

--- inventory-app/src/main/java/inventory/InvalidItem.java
+++ inventory-app/src/main/java/inventory/InvalidItem.java
@@ -1,5 +1,10 @@
 package inventory;
 
+/**
+ * This Runtime exception is used for indicating invalid Items.
+ *
+ * The exceptions contains a message explaining why the item is invalid.
+ */
 public class InvalidItem extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
